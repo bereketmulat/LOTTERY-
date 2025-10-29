518b81aaf889b8a277fe688a630722f56e7c20c7
Bereket Mulat Mengiste <bereketmulat59@gmail.com>
2025-10-28 23:05:58 -0400

    Add files via upload

---
+++ pages/cities.js
@@ -0,0 +1,116 @@
+const cities = () => {
+  return (
+    <div className="about">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">TOURIST</a>
+          </li>
+          <li>
+            <a href="#">VISITING SITES</a>
+          </li>
+          <li>
+            <a href="#">HOTELS</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="AboutTheCity">
+        <h1>ABOUT DUBAI</h1>
+
+        <p>
+          Dubai, city and capital of the emirate of Dubai, one of the wealthiest
+          of the seven emirates that constitute the federation of the United
+          Arab Emirates, which was created in 1971 following independence from
+          Great Britain. There are several theories about the origin of the name
+          Dubai. One associates it with the daba—a type of locust that infests
+          the area—while another holds that it refers to a market that existed
+          near the city. In recent years Dubai has been compared to Singapore
+          and Hong Kong and is often regarded as the Middle East’s premier
+          entrepôt. Area 13.5 square miles (35 square km). Pop. (2017 est.)
+          2,919,178. Palm Jumeirah Palm JumeirahPalm Jumeirah, Dubai, United
+          Arab Emirates, photographed from the International Space Station,
+          2005. The western area of Dubai benefits from small stretches of sandy
+          beaches, which have helped to catalyze the city’s tourism industry.
+          Dubai’s rulers have sought to increase the city’s limited seafronts,
+          and, in the absence of natural offshore islands, developers were
+          encouraged to construct giant man-made islands off the coast of the
+          city. The most famous of these is Palm Jumeirah, which has the shape
+          of a palm tree. Others include the “World” islands, a cluster of small
+          islands positioned to resemble a world map when viewed from above.
+          City site and layout
+        </p>
+
+        <p>
+          The new city centre is a string of skyscrapers lining Sheikh Zayed
+          Road. Notable among these are the Emirates Towers, which were built in
+          the late 1990s and early 2000s and which house a hotel and government
+          offices. Close to Sheikh Zayed Road is the Dubai International
+          Financial Centre, housed in a futuristic arch-shaped building, and the
+          Burj Khalifa, which at the time of its official opening in 2010 was
+          the world’s tallest building; it was named after the president of the
+          United Arab Emirates and emir of Abu Dhabi, Khalifa ibn Zayed Al
+          Nahyan. To the west of the skyscrapers lie several affluent suburbs,
+          most of which house substantial villas. On their periphery lies the
+          Burj al-ʿArab, a giant sail-shaped tower which is home to a luxury
+          hotel. Farther west are new clusters of skyscrapers surrounding a
+          man-made marina and several man-made lakes.
+        </p>
+      </div>
+
+      <div className="PopularAttractions">
+        <h1>POPULAR ATTRACTIONS</h1>
+      </div>  
+
+        
+      <div className="cityCard">
+         <div className="Attractionpictures1">
+              <img src="https://www.hindustantimes.com/ht-img/img/2023/07/27/900x1600/Dubai_1690446969389_1690446969630.jpg"></img>
+              <h1>Burj Khalifa</h1>
+            <p>
+              The Burj Khalifa is the tallest building in the world, soaring
+              2,717 feet (828 meters) high with more than 160 stepped floors
+              that narrow as it climbs syringe-like to the sky. Designed to pay
+              homage to the complex geometry of Islamic art, the Burj Khalifa
+              brings a new meaning to the term skyscraper and is a highlight of
+              the massive downtown Dubai complex.
+            </p>
+         </div>
+      
+          
+
+
+          <div className="Attractionpictures2">
+            <img src="https://dynamic-media.tacdn.com/media/attractions-content--1x-1/15/10/3d/52.jpg?f=webp&w=400&h=300&s=1"></img>
+            <h1>Dubai Marina</h1>
+            <p>
+              Carved along the Gulf shoreline south of Dubai’s The Palm island,
+              Dubai Marina is a skyscraper-packed waterfront community that’s
+              one of the city’s swishest residential and leisure hotspots. At
+              its heart is a 2-mile (3-kilometer) waterway framed by residential
+              blocks, hotels, shops, restaurants, and entertainment venues.
+            </p>
+          </div>
+
+          <div className="Attractionpictures3">
+            <img src="https://dynamic-media.tacdn.com/media/attractions-content--1x-1/15/10/04/4f.jpg?f=webp&w=400&h=300&s=1"></img>
+            <h1>Burj Al-Arab Jumeirah</h1>
+            <p>
+              The sail-like silhouette of the Burj Al-Arab Jumeirah has become a
+              symbol of Dubai's opulence and affluence, standing at 1,053 feet
+              (321 meters) as the third-tallest hotel and one of the most
+              luxurious hotels in the United Arab Emirates. Opened in 1999, the
+              landmark boasts plenty of superlatives, including its seven-star
+              status.
+            </p>
+            
+        </div>
+    </div>
+    </div>
+  );
+};
+
+export default cities;
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+const HomePage = () => {
+  return (
+    
+  <div className = "home">  
+
+<h1> Welcome to Dubai </h1>
+<p> Going to Dubai represents a different way of life  </p>
+<a href = "\cities"> Learn more</a>
+  
+  </div>
+  
+  
+   
+  );
+  }
+
+
+   export default HomePage
